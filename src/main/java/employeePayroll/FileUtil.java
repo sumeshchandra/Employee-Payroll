1f3c628b8f5f7e93db407cb5e7ea46952fe09ca3
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-08-19 21:02:28 +0530

    Added: Demonstrate file Operation like

---
+++ src/main/java/employeePayroll/FileUtil.java
@@ -0,0 +1,16 @@
+package employeePayroll;
+
+import java.io.File;
+
+public class FileUtil {
+
+    public static boolean deleteFiles(File contentsToDelete) {
+        File[] allContents = contentsToDelete.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+}
